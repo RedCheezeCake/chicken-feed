f0bf0c91348a23a866327175e13ecd6c92c4e1c9
김대호 <donald@leadsoft.co.kr>
2019-04-28 00:57:32 +0900

    + 프로젝트 초기화

---
+++ src/main/java/me/donald/lotto/LottoApplication.java
@@ -0,0 +1,13 @@
+package me.donald.lotto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LottoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LottoApplication.class, args);
+	}
+
+}
