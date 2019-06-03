e36311bde1fe49a84b21cb2e3639a1a8406b4dc5
Dae-Ho Kim <rlaeogh222@gmail.com>
2019-06-03 18:44:41 +0900

    Win 관련 테스트 추가
    - 정상
    - 범위 에러
    - 상태 에러(해당 회차 당첨금을 입력하지 않았을 시)

---
+++ src/test/java/me/donald/chickenfeed/domain/win/WinTest.java
@@ -0,0 +1,76 @@
+package me.donald.chickenfeed.domain.win;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+public class WinTest {
+
+	/**
+	 * 1, 2, 3 등에 해당하는 당첨금 입력
+	 */
+	@Test
+	public void setRankPrizes_normal() {
+		// given
+		Win win = new Win(0, Arrays.asList(1, 2, 3, 4, 5, 6, 7), 10000);
+		List<Long> prizes = Arrays.asList(100L, 10L, 1L);
+
+		// when
+		win.setRankPrizes(prizes);
+
+		// then
+		assertThat(win.getRankPrize(1)).isEqualTo(100L);
+		assertThat(win.getRankPrize(2)).isEqualTo(10L);
+		assertThat(win.getRankPrize(3)).isEqualTo(1L);
+		assertThat(win.getRankPrize(4)).isEqualTo(50000L);
+		assertThat(win.getRankPrize(5)).isEqualTo(5000L);
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void setRankPrizes_exceptionRange() {
+		// given
+		Win win = new Win(0, Arrays.asList(1, 2, 3, 4, 5, 6, 7), 10000);
+		List<Long> prizes = Arrays.asList(100L, 10L, 1L, 0L);
+
+		// when
+		win.setRankPrizes(prizes);
+
+		// then
+	}
+
+	/**
+	 * 등수에 따른 당첨금 조회
+	 */
+	@Test(expected = IllegalArgumentException.class)
+	public void getRankPrize_exceptionRange() {
+		// given
+		Win win = new Win(0, Arrays.asList(1, 2, 3, 4, 5, 6, 7), 10000);
+		List<Long> prizes = Arrays.asList(100L, 10L, 1L);
+
+		// when
+		win.getRankPrize(0);
+
+		// then
+	}
+
+	@Test(expected = IllegalStateException.class)
+	public void getRankPrize_exceptionNotPreparedState() {
+		// given
+		Win win = new Win(0, Arrays.asList(1, 2, 3, 4, 5, 6, 7), 10000);
+
+		// when
+		win.getRankPrize(1);
+
+		// then
+	}
+}
