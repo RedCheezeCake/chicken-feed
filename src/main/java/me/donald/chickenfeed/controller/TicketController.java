4fe19a70156e856bfeab0934fd999c2dc50e3dfc
Dae-Ho Kim <rlaeogh222@gmail.com>
2019-06-09 17:09:14 +0900

    최근 생성된 티켓 리스트 조회 api 추가
    - 최근 생생성된 티켓을 조회하는 api 추가
    - 응답 모델 추가
    - 관련 환경 변수 추가

--- src/main/java/me/donald/chickenfeed/controller/TicketController.java
+++ src/main/java/me/donald/chickenfeed/controller/TicketController.java
@@ -1,18 +1,40 @@
 package me.donald.chickenfeed.controller;
 
+import com.querydsl.jpa.impl.JPAQuery;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import me.donald.chickenfeed.domain.ticket.QTicket;
+import me.donald.chickenfeed.domain.ticket.Ticket;
 import me.donald.chickenfeed.model.Numbers;
+import me.donald.chickenfeed.model.response.TicketStream;
+import me.donald.chickenfeed.model.response.TicketStreamElement;
 import me.donald.chickenfeed.service.application.TicketService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.persistence.EntityManager;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class TicketController {
 
+	@Value("${ticket.stream.size.init}")
+	private int TICKET_STREAM_INIT_SIZE;
+
+	@Value("${ticket.stream.size.maximum}")
+	private int TICKET_STREAM_MAX_SIZE;
+
 	private final TicketService ticketService;
+	private final EntityManager entityManager;
 
 	@PostMapping("/tickets")
 	public ResponseEntity createTicket(@RequestBody Numbers reqNumbers) {
@@ -21,5 +43,40 @@ public class TicketController {
 
 		Integer[] numbers = reqNumbers.getNumbers();
 		return ResponseEntity.ok(ticketService.createTicket(numbers));
+	}
+
+	@GetMapping("/tickets")
+	public ResponseEntity findLastTickets(@RequestParam(name = "lastTicketNo", required = false) Long previousLastTicketNo) {
+		log.debug("Previous Last Ticket No : '{}'", previousLastTicketNo);
+
+		QTicket qTicket = QTicket.ticket;
+		List<Ticket> ticketList;
+		if (previousLastTicketNo == null) {
+			ticketList = new JPAQuery<>(entityManager)
+					.select(qTicket)
+					.from(qTicket)
+					.orderBy(qTicket.ticketNo.desc())
+					.limit(TICKET_STREAM_INIT_SIZE)
+					.fetch();
+		} else {
+			ticketList = new JPAQuery<>(entityManager)
+					.select(qTicket)
+					.from(qTicket)
+					.orderBy(qTicket.ticketNo.desc())
+					.where(qTicket.ticketNo.gt(previousLastTicketNo))
+					.limit(TICKET_STREAM_MAX_SIZE)
+					.fetch();
+		}
+
+		if (ticketList.isEmpty())
+			return ResponseEntity.ok(new TicketStream(previousLastTicketNo, Collections.emptyList()));
+
+		List<TicketStreamElement> elements = ticketList.stream()
+				.map(ticket -> new TicketStreamElement(ticket.getTicketNumbers(), ticket.getIssueTime()))
+				.collect(Collectors.toList());
+
+		long currentLastTicketNo = ticketList.get(0).getTicketNo();
+
+		return ResponseEntity.ok(new TicketStream(currentLastTicketNo, elements));
 	}
 }
